ccd25da7eca5bea2923976c3810d87a0d1665348
Kajsa Asp <kajsalisaasp@gmail.com>
2024-10-22 16:00:41 +0200

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express(),
   port = process.env.PORT || 3000
+
+  app.use(cors())
+
+  app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+
+  app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+  })
 
 app.get('/', (_request, response) => {
   response.send({ hello: 'World' })
